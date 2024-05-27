abc36b22e9876bae4761d892ac5d43af52138aff
smasha11 <ismail.hussein.1120@gmail.com>
2024-05-27 11:19:37 +0300

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', function(){
+   let newparagraph = document.getElementById('text')
+   newparagraph.textContent = "This is really cool!"
+});
